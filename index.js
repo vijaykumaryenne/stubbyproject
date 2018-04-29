4a06ae2c147cad4414083f3651e76b5dab299f33
vijaykumaryenne <vijaykumar.yenne@oracle.com>
2018-04-29 13:55:43 +1000

    v13

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
-
+const PORT = process.env.PORT || 5000
 var Components = require('./components.js');
 
 // Create a server instance
 var server = Components('/components');
 
 // Start the server listening..
-server.listen(process.env.SERVICE_PORT || 7950);
+server.listen(PORT || 5000);
